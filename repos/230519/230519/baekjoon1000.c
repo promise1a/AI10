059c18f6d17d3a035db9a3aaac1125cb9aa5dd1e
YH <promise1a@naver.com>
2023-05-19 15:25:16 +0900

    repos

--- repos/230519/230519/baekjoon1000.c
+++ repos/230519/230519/baekjoon1000.c
@@ -1,13 +1,22 @@
+//두 정수 A와 B를 입력받은 다음,
+//A+B를 출력하는 프로그램을 작성하시오.
+
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+//첫째 줄에 A+B를 출력한다.
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
+	int A, B;
 
-	int a, b;
+	scanf("%d %d", &A, &B);
 
-	scanf("%d %d", &a, &b);
+	int result = A + B;
 
-	printf("");
+	printf("%d", result);
+
 
 }
---
+++ repos/230519/230519/baekjoon2754_comment.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+
+	// 2개의 문자로 이루어진 학점을 평점으로 변경하여 출력하라.
+	
+	// 1. 첫번째 값 입력받기
+	// c1 변수 예상값 :: A, B, C, D, F
+
+	char c1;
+	scanf("%c", &c1);
+
+	// c1의 값이 F라면 결과 즉시 출력,
+	// 아니라면 입력 및 연산을 이어서 수행
+
+	if (c1 == 'F') {printf("0.0\n");}
+	else {
+		char c2;
+		scanf("%c", &c2);
+
+		double score = 0;
+		if (c1 == 'A') {score = 4;}
+		else if (c1 == 'B') {score = 3;}
+		else if (c1 == 'C') {score = 2;}
+		else {score = 1;}
+
+		if (c2 == '+') {score += 0.3;}
+		else if (c2 == '-') {score -= 0.3;}
+
+		printf("%.1f", score);
+	}
+
+
+
+
+}
